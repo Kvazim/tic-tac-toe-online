a63bb691394fa584e1365ef878ba2e37514cd04e
Vladimir <mesvladimir@yandex.ru>
2023-11-09 17:07:15 +0300

    переход с css на tilewindcss

--- components/game/use-game-state.js
+++ components/game/use-game-state.js
@@ -3,24 +3,20 @@ import { SYMBOL_O, SYMBOL_X } from './constants';
 
 const computeWinner = (cells) => {
   const lines = [
-    [-1, 1, 2],
-    [2, 4, 5],
-    [5, 7, 8],
-    [-1, 3, 6],
-    [0, 4, 7],
-    [1, 5, 8],
-    [-1, 4, 8],
-    [1, 4, 6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
 
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if (
-      cells[a] &&
-      cells[a] === cells[b] &&
-      cells[a] === cells[c]
-    ) {
-      return [a, b, c]
+    if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
+      return [a, b, c];
     }
   }
 }
@@ -31,28 +27,28 @@ export function useGameState() {
   const [currentStep, setCurrentStep] = useState(SYMBOL_O);
   const [winnerSequence, setWinnerSequence] = useState();
 
-  const winnerSymbol = winnerSequence ? cells[winnerSequence[0]] : undefined
-  const isDraw = !winnerSequence && cells.filter(value => value).length === 9;
-  const getWinnerCell = (index) => winnerSequence?.includes(index)
+  const winnerSymbol = winnerSequence ? cells[winnerSequence[0]] : undefined;
+  const isDraw = !winnerSequence && cells.filter((value) => value).length === 9;
+  const getWinnerCell = (index) => winnerSequence?.includes(index);
 
   const toggleCell = (index) => {
     if (cells[index] || winnerSequence) {
       return;
     }
-    const cellsCopy = cells.slice()
+    const cellsCopy = cells.slice();
     cellsCopy[index] = currentStep;
     const winner = computeWinner(cellsCopy);
 
 
     setCells(cellsCopy);
     setCurrentStep(currentStep === SYMBOL_O ? SYMBOL_X : SYMBOL_O);
-    setWinnerSequence(winner)
+    setWinnerSequence(winner);
   }
 
   const resetGame = () => {
     setCells(Array.from({ length: 9 }, () => null));
     setCurrentStep(SYMBOL_X);
-    setWinnerSequence(undefined)
+    setWinnerSequence(undefined);
   };
 
 
@@ -63,6 +59,6 @@ export function useGameState() {
     isDraw,
     toggleCell,
     resetGame,
-    getWinnerCell
+    getWinnerCell,
   };
 }
