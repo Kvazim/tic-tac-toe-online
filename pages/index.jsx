2c8b5c31979e31c58de19978734ebbdf4a91738f
Владимир <mesvladimir@gmail.com>
2023-11-16 21:02:13 +0300

    feat: Добавление модалки

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,6 +6,8 @@ import {
   useGameState
 } from '../components/game';
 import { Header } from '../components/header';
+import { UiModal } from '../components/uikit/ui-modal';
+import { UiButton } from '../components/uikit/ui-button';
 
 
 export default function HomePage() {
@@ -24,6 +26,16 @@ export default function HomePage() {
           onPlayerTimeOver={handlePlayerTimeOver}
           className="mt-4"
         />
+        <UiModal width="md" isOpen={winnerSymbol} onClose={() => console.log("close")}>
+          <UiModal.Header>Игра завершена</UiModal.Header>
+          <UiModal.Body>
+            <p className="text-sm">Победитель: <span className="text-teal-600">Paromovevg</span></p>
+          </UiModal.Body>
+          <UiModal.Footer>
+            <UiButton size="md" variant="outline">Вернуться</UiButton>
+            <UiButton size="md" variant="primary">Играть снова</UiButton>
+          </UiModal.Footer>
+        </UiModal>
         <GameField
           cells={cells}
           currentMove={currentMove}
