19d8e94a9ba4a91dff8fb526c036d3d8ea721785
Владимир <mesvladimir@gmail.com>
2023-11-16 13:47:08 +0300

    feat: Добавление таймера хода

--- components/game/game-info.jsx
+++ components/game/game-info.jsx
@@ -1,6 +1,5 @@
 import clsx from "clsx";
 import { Profile } from "../profile";
-import { CrossIcon } from "./icons/cross-icon";
 import { GameSymbol } from "./game-symbol";
 import { GAME_SYMBOLS } from "./constants";
 
@@ -8,6 +7,8 @@ import avatarSrc1 from "./images/avatar-1.png"
 import avatarSrc2 from "./images/avatar-2.png"
 import avatarSrc3 from "./images/avatar-3.png"
 import avatarSrc4 from "./images/avatar-4.png"
+import { useEffect, useState } from "react";
+import internal from "stream";
 
 const players = [
   {
@@ -22,35 +23,68 @@ const players = [
     name: "Paromovevg",
     rating: "1230",
     avatar: avatarSrc2,
-    symbol: GAME_SYMBOLS.CROSS,
+    symbol: GAME_SYMBOLS.SQUARE,
   },
   {
     id: 3,
     name: "KJlfskfdhlksdhfsdlfkhdshldfsdkdjfklsd",
     rating: "1230",
     avatar: avatarSrc3,
-    symbol: GAME_SYMBOLS.CROSS,
+    symbol: GAME_SYMBOLS.TRIANGLE,
   },
   {
     id: 4,
     name: "Paromovevg",
     rating: "1230",
     avatar: avatarSrc4,
-    symbol: GAME_SYMBOLS.CROSS,
+    symbol: GAME_SYMBOLS.ZERO,
   },
 ];
 
-export function GameInfo({ className, playersCount }) {
+export function GameInfo({ className, playersCount, currentMove }) {
+
   return (
     <div className={clsx("grid grid-cols-2 gap-3 justify-between bg-white rounded-2xl shadow-md px-8 py-4", className)}>
       {
-        players.slice(0, playersCount).map((player, index) => <PlayerInfo playerInfo={player} key={player.id} isRight={index % 2 === 1} />)
+        players.slice(0, playersCount).map((player, index) => (
+          <PlayerInfo
+            playerInfo={player}
+            key={player.id}
+            isRight={index % 2 === 1}
+            isTimerRunning={currentMove === player.symbol}
+          />
+        ))
       }
     </div>
   );
 }
 
-function PlayerInfo({playerInfo, isRight}) {
+function PlayerInfo({playerInfo, isRight, isTimerRunning}) {
+  const [seconds, setSeconds] = useState(60);
+  const minutesString = String(Math.floor(seconds / 60)).padStart(2, "0");
+  const secondString = String(seconds % 60).padStart(2, "0");
+  const isDanger = seconds < 10;
+
+  const getTimerRunning = () => {
+    if (isTimerRunning) {
+      return isDanger ? "text-orange-600" : "text-slate-900";
+    }
+    return "text-slate-200";
+  }
+
+  useEffect(()=> {
+    if (isTimerRunning) {
+      const interval = setInterval(()=> {
+        setSeconds((s) => Math.max(s-1, 0))
+      }, 1000);
+
+      return () => {
+        clearInterval(interval);
+        setSeconds(60);
+      }
+    }
+  }, [isTimerRunning]);
+
   return (
     <div className="flex gap-3 items-center">
       <div className={clsx("relative", isRight && "order-3")}>
@@ -65,7 +99,7 @@ function PlayerInfo({playerInfo, isRight}) {
         </div>
       </div>
       <div className={clsx("h-6 w-px bg-slate-200", isRight && "order-2")}></div>
-      <div className={clsx("text-slate-900 text-lg font-semibold", isRight && "order-1")}>01:08</div>
+      <div className={clsx("text-lg font-semibold w-[60px]", isRight && "order-1", getTimerRunning())}>{minutesString}:{secondString}</div>
     </div>
   );
 }
