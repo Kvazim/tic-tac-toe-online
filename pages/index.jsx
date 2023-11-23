269244d4251dd32d6c61d1f616422a8b8b707188
Владимир <mesvladimir@gmail.com>
2023-11-23 17:54:09 +0300

    feat: Добавление селекта и инпута

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,24 @@
 import { Header } from '../components/header';
 import { Game } from '../components/game-new';
+import { UiTextField } from "../components/uikit/fields/ui-text-field";
+import { UiSelectField } from "../components/uikit/fields/ui-select-field";
 
 
 export default function HomePage() {
   return (
     <HomePageLayout header={<Header />}>
-      <Game />
+      {/* <Game /> */}
+      <UiSelectField
+        label="Label"
+        placeholder="Placeholder"
+        required
+        helperText="Helper text"
+        errorText="Error text"
+        options={[
+          { label: "Первый label", value: 1 },
+          { label: "Второй label", value: 2 },
+        ]}
+      />
     </HomePageLayout>
   );
 }
